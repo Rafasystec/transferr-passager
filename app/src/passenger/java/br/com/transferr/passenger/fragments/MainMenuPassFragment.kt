35613a650c91b8d3879b153d0a4d1de1a802b1a7
rafasystec <rafasystec@yahoo.com.br>
2019-05-21 11:07:57 -0300

    For Rate app functionality.

--- app/src/passenger/java/br/com/transferr/passenger/fragments/MainMenuPassFragment.kt
+++ app/src/passenger/java/br/com/transferr/passenger/fragments/MainMenuPassFragment.kt
@@ -11,6 +11,16 @@ import br.com.transferr.extensions.callEmailHost
 import br.com.transferr.extensions.setupToolbar
 import br.com.transferr.fragments.SuperClassFragment
 import kotlinx.android.synthetic.passenger.fragment_pass_main_menu.*
+import android.widget.Toast
+import android.content.ActivityNotFoundException
+import android.net.Uri
+import br.com.transferr.extensions.toast
+import hotchemi.android.rate.AppRate
+import br.com.transferr.passenger.activities.MainActivity
+import hotchemi.android.rate.OnClickButtonListener
+
+
+
 
 /**
  * A simple [Fragment] subclass.
@@ -27,6 +37,8 @@ class MainMenuPassFragment : SuperClassFragment() {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar(R.id.toolbar,getString(R.string.menu))
 
+        activeFrameWorkToMonitorRateApp()
+
         btnMainMenuContact.setOnClickListener {
             callEmailHost("contact.boora@gmail.com",getString(R.string.emailSubject),getString(R.string.emailBody),getString(R.string.emailTitle))
         }
@@ -34,14 +46,11 @@ class MainMenuPassFragment : SuperClassFragment() {
         btnMainMenuTerms.setOnClickListener {
             activity?.startActivity(Intent(activity, TermsOfUseActivity::class.java))
         }
-        /*
-        if(BuildConfig.FLAVOR == "driver"){
-            btnLogout.visibility = View.VISIBLE
-            btnLogout.setOnClickListener { logout() }
-        }else{
-            btnLogout.visibility = View.GONE
+        btnMainMenuRateApp.setOnClickListener {
+            //rateTheAppOnStore()
+            AppRate.with(activity).showRateDialog(activity)
         }
-        */
+
     }
 
 /*    private fun logout(){
@@ -58,6 +67,34 @@ class MainMenuPassFragment : SuperClassFragment() {
 
     }
 */
+    private fun rateTheAppOnStore() {
+        val uri = Uri.parse("market://details?id=br.com.idoctorbrasil.idoctor_android")// + activity!!.packageName)
+        val myAppLinkToMarket = Intent(Intent.ACTION_VIEW, uri)
+        try {
+            startActivity(myAppLinkToMarket)
+        } catch (e: ActivityNotFoundException) {
+            toast( getString(R.string.unableToFindApp ) )
+        }
+
+    }
+
+    private fun activeFrameWorkToMonitorRateApp(){
+        AppRate.with(activity)
+                .setInstallDays(0) // default 10, 0 means install day.
+                .setLaunchTimes(3) // default 10
+                .setRemindInterval(2) // default 1
+                .setShowLaterButton(true) // default true
+                .setDebug(false) // default false
+                //.setTextRateNow(R.string.appRateText)
+                .setOnClickButtonListener { which ->
+                    // callback listener.
+                    //Log.d(MainActivity::class.java.name, Integer.toString(which))
+                }
+                .monitor()
+
+        // Show a dialog if meets conditions
+        AppRate.showRateDialogIfMeetsConditions(activity)
+    }
 
 
 }// Required empty public constructor
